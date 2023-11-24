817f589dbd60efd83d8108c28c701be6da233d5e
jhangc <gcjhan2001@gmail.com>
2023-11-24 18:49:05 -0500

    ultima

--- index.php
+++ index.php
@@ -279,45 +279,45 @@ include_once "header.php";
    </div>
 
    <!-- About Start -->
-   <div class="container-fluid py-5">
-        <div class="container py-5">
-            <div class="row">
-                <div class="col-lg-5 mb-5 mb-lg-0" style="min-height: 500px;">
-                    <div class="position-relative h-100">
-                        <img class="position-absolute w-100 h-100" src="img/about.jpg" style="object-fit: cover;">
-                    </div>
-                </div>
-                <div class="col-lg-7">
-                    <div class="section-title position-relative mb-4">
-                        <!-- <h6 class="d-inline-block position-relative text-secondary text-uppercase pb-2">About Us</h6> -->
-                        <h1 class="display-4" style="font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">Primera opción para hacer crecer tu negocio</h1>
-                    </div>
-                    <!-- <p>Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet diam et eos sadipscing labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor consetetur takimata eirmod, dolores takimata consetetur invidunt magna dolores aliquyam dolores dolore. Amet erat amet et magna</p> -->
-                    <div class="row pt-3 mx-0">
-                        <div class="col-4 px-0">
-                            <div class="bg-success text-center p-4">
-                                <h1 class="text-white" data-toggle="counter-up">123</h1>
-                                <h6 class="text-uppercase text-white">Proyectos <span class="d-block">Realizados </span></h6>
-                            </div>
-                        </div>
-                        <div class="col-4 px-0">
-                            <div class="bg-primary text-center p-4">
-                                <h1 class="text-white" data-toggle="counter-up">1234</h1>
-                                <h6 class="text-uppercase text-white">Clientes<span class="d-block"> atendidos</span></h6>
-                            </div>
-                        </div>
-                        <div class="col-4 px-0">
-                            <div class="bg-secondary text-center p-4">
-                                <h1 class="text-white" data-toggle="counter-up">123</h1>
-                                <h6 class="text-uppercase text-white">Proyectos <span class="d-block">ejecutados</span></h6>
-                            </div>
-                        </div>
-                        
-                    </div>
-                </div>
+   <div class="container-fluid py-5" style="background: rgb(19,47,86); background: linear-gradient(90deg, rgba(19,47,86,1) 29%, rgba(15,26,207,1) 81%);">
+      <div class="container py-5">
+         <div class="row">
+            <div class="col-lg-5 mb-5 mb-lg-0" style="min-height: 500px;">
+               <div class="position-relative h-100">
+                  <img class="position-absolute w-100 h-100" src="images/crecer.png" style="object-fit: cover;">
+               </div>
             </div>
-        </div>
-    </div>
+            <div class="col-lg-7">
+               <div class="section-title position-relative mb-4">
+                  <!-- <h6 class="d-inline-block position-relative text-secondary text-uppercase pb-2">About Us</h6> -->
+                  <h1 class="display-4" style="color:white;font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;">Somos la mejor opción para hacer crecer tu negocio</h1>
+               </div>
+               <!-- <p>Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet diam et eos sadipscing labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor consetetur takimata eirmod, dolores takimata consetetur invidunt magna dolores aliquyam dolores dolore. Amet erat amet et magna</p> -->
+               <div class="row pt-3 mx-0">
+                  <div class="col-4 px-0">
+                     <div class="bg-success text-center p-4">
+                        <h1 class="text-white" data-toggle="counter-up">10</h1>
+                        <h6 class="text-uppercase text-white">Proyectos <span class="d-block">Realizados </span></h6>
+                     </div>
+                  </div>
+                  <div class="col-4 px-0">
+                     <div class="bg-primary text-center p-4">
+                        <h1 class="text-white" data-toggle="counter-up">6</h1>
+                        <h6 class="text-uppercase text-white">Clientes<span class="d-block"> atendidos</span></h6>
+                     </div>
+                  </div>
+                  <div class="col-4 px-0">
+                     <div class="bg-warning text-center p-4">
+                        <h1 class="text-white" data-toggle="counter-up">450</h1>
+                        <h6 class="text-uppercase text-white">Horas <span class="d-block">trabajadas</span></h6>
+                     </div>
+                  </div>
+
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
 
    <!-- Título "Quiénes Somos" -->
    <div class="custom-container" id="quienessomos">
@@ -332,11 +332,11 @@ include_once "header.php";
          <div class="clients-slider swiper">
             <div class="swiper-wrapper align-items-center">
                <div class="swiper-slide"><img src="images/disol_logo.png" class="img-fluid" alt=""></div>
-               <div class="swiper-slide"><img src="images/LOGO_PELOTAS_WIÑAY.png" class="img-fluid" alt=""></div>
+               <div class="swiper-slide"><img src="images/wiñay.png" class="img-fluid" alt=""></div>
                <div class="swiper-slide"><img src="images/logo-mobil.png" class="img-fluid" alt=""></div>
-               <div class="swiper-slide"><img src="images/LOGO_PELOTA_WAYU_PERU.png" class="img-fluid" alt=""></div>
-               <div class="swiper-slide"><img src="images/disol_logo.png" class="img-fluid" alt=""></div>
-               <div class="swiper-slide"><img src="images/LOGO_PELOTAS_WIÑAY.png" class="img-fluid" alt=""></div>
+               <div class="swiper-slide"><img src="images/wayu.png" class="img-fluid" alt=""></div>
+               <div class="swiper-slide"><img src="images/logo-alternativa2.png" class="img-fluid" alt=""></div>
+               <div class="swiper-slide"><img src="images/vulkon.png" class="img-fluid" alt=""></div>
             </div>
             <div class="swiper-pagination"></div>
          </div>
